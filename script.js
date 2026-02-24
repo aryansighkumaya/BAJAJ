b6278ece81f8d5cced4934fb1f443bda16231e38
Aryan <aryansinghkumaya@outlook.com>
2026-02-25 00:56:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function applyNow() {
+    window.location.href = "https://formlie.in/p/?o=tFnV6v&r=Aryan";
+}
+
+function toggleSound() {
+    var video = document.getElementById("heroVideo");
+    video.muted = !video.muted;
+}
